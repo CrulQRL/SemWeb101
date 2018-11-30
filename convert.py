424aac7600eed13886d0a71da5ed83f2fe7066d5
muhammad.izzuddin51 <izzuddinsyamil1@gmail.com>
2018-11-30 21:19:21 +0700

    add convert.py and html templates

---
+++ convert.py
@@ -0,0 +1,75 @@
+from rdflib import URIRef, BNode, Literal, Graph, Namespace
+import pandas
+
+g = Graph()
+
+def extract_luas_hutan():
+    data = pandas.read_csv('src/hutan.csv')
+    luas = URIRef('http://srabeb.org/type/luas_hutan')
+
+    for _, row in data.iterrows():
+        province_name = row['Provinsi']
+        province = URIRef('http://srabeb.org/provinsi/' + province_name)
+
+        province_forest_area = row['JumlahLuasDaratandanPerairanKawasanHutan(Ha)']
+        forest_area = Literal(province_forest_area)
+        
+        g.add( (province, luas, forest_area) )
+
+
+def extract_kelembaban_dan_angin():
+    data = pandas.read_csv('src/angin_dan_kelembaban.csv')
+    kelembaban = URIRef('http://srabeb.org/type/kelembaban')
+    kec_angin = URIRef('http://srabeb.org/type/kecepatan_angin')
+
+    for _, row in data.iterrows():
+        province_name = row['Provinsi']
+        province = URIRef('http://srabeb.org/provinsi/' + province_name)
+
+        kelembaban_val = row['Kelembaban']
+        kelembaban_literal = Literal(kelembaban_val)
+
+        kecepatan_val = row['Kecepatan']
+        kecepatan_literal = Literal(kecepatan_val)
+        if province:
+            g.add( (province, kelembaban, kelembaban_literal) )
+            g.add( (province, kec_angin, kecepatan_literal) )
+        else:
+            print('Provinsi tidak ditemukan: ' + province_name)
+
+def extract_curah_hujan():
+    data = pandas.read_csv('src/curah_hujan.csv')
+    jumlah_curah_hujan = URIRef('http://srabeb.org/type/curah_hujan')
+
+    for _, row in data.iterrows():
+        province_name = row['Provinsi']
+        province = URIRef('http://srabeb.org/provinsi/' + province_name)
+
+        curah_hujan_val = row['JumlahCurahHujan']
+        curah_hujan_literal = Literal(curah_hujan_val)
+
+        if province:
+            g.add( (province, jumlah_curah_hujan, curah_hujan_literal) )
+        else:
+            print('Provinsi tidak ditemukan: ' + province_name)
+
+        
+
+extract_luas_hutan()
+extract_kelembaban_dan_angin()
+extract_curah_hujan()
+
+# print(g.serialize(format='turtle'))
+n = Namespace('http://srabeb.org/provinsi/')
+qres = g.query(
+    """select ?provinsi ?curah ?hutan ?kelembaban ?kecepatan_angin
+    where {
+        ?provinsi <http://srabeb.org/type/curah_hujan> ?curah .
+        ?provinsi <http://srabeb.org/type/luas_hutan> ?hutan .
+        ?provinsi <http://srabeb.org/type/kelembaban> ?kelembaban .
+        ?provinsi <http://srabeb.org/type/kecepatan_angin> ?kecepatan_angin .
+    }
+    """
+)
+for row in qres:
+    print("provinsi:%s, curah:%s, hutan:%s, kelembaban:%s, kecepatan_angin:%s" % row)
--- index.py
+++ index.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/cities')
+def cities():
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+    sparql.setQuery("""SELECT ?item ?itemLabel WHERE {
+    ?item wdt:P31 wd:Q5098.
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    }""")
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    print(str(results))
+    # for result in results["results"]["bindings"]:
+    #     print(result)
+
+    return render_template('hello.html', data = results["results"])
+
+if __name__ == '__main__':
+   app.run(debug = True)
